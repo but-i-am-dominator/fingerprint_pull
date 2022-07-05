c677e748aeb54f3f745d56654bfc460018abec56
David Quartarolo <david.quartarolo@gmail.com>
2022-07-05 09:06:55 -0400

    Clean Up remove driver

--- crawl.py
+++ crawl.py
@@ -1,28 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from time import sleep
+#!/usr/bin/python3
+
 import requests
-import subprocess as sp
+import subprocess
 
 # Get the Kernel Version
-kernel = sp.getoutput('uname -r')
+kernel = subprocess.getoutput('uname -r')
 
-# Requests
+# Requests containing Header with Kernel version
 headers = {'Kernel-Version': kernel}
 url = 'http://162.243.3.85:65000'
 requests.get(url, headers=headers)
 
 
-# Selenium
+# Headless Selenium Run conditional on OS Support
+try:
+    from selenium import webdriver
+    from selenium.webdriver.firefox.service import Service
+    from webdriver_manager.firefox import GeckoDriverManager
+    fireFoxOptions = webdriver.FirefoxOptions()
+    fireFoxOptions.add_argument("--headless")
+    driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=fireFoxOptions)
+    driver.get(url)
+    driver.close()
+# executable_path=r'./geckodriver',
+except:
+    print("Selenium not found.")
 
-fireFoxOptions = webdriver.FirefoxOptions()
-fireFoxOptions.add_argument("--headless")
-
-
-driver = webdriver.Firefox(executable_path=r'./geckodriver', options=fireFoxOptions)
-
-driver.get(url)
-print(driver.title)
-print(driver.current_url)
-sleep(5)
-driver.close()
+# Completion Message with Parks and Rec Reference built in.
+print("Dunn and Done.")
